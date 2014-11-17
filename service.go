d46865ec16e6506905942b4b6fd02c2443e9d80f
tanxr <tanxrong@gmail.com>
2014-11-17 11:20:09 +0800

    add simple log

--- service.go
+++ service.go
@@ -2,13 +2,16 @@ package penny
 
 import (
 	"sync"
+	"log"
+	"os"
 	luar "github.com/stevedonovan/luar"
 )
+
 type Service interface {
-	func Name() string
-	func Open() *Service
-	func Init(param string) bool
-	func Close()
+	Name() string
+	Open() Service
+	Call(m Msg) error
+	Close()
 }
 
 type Msg struct {
@@ -19,8 +22,33 @@ type Msg struct {
 
 type Center struct {
 	storage map[string]Service
+	instances []*Service
+	mutex sync.Mutex
+	id uint16
 }
 
-type Msgqueue struct {
-	ch chan Msg
+type Slog struct {
+	l *log.Logger
+}
+
+func (s *Slog) Name() string {
+	return "logger"
+}
+
+func (s *Slog) Open() Slog {
+	file,err := os.OpenFile("/tmp/penny.log",os.O_WRONLY | os.O_CREATE ,0644)
+	if err != nil {
+		panic(err)
+	}
+	l := log.New(file,"[test]",log.Ldate | log.Ltime | log.Lshortfile)
+	return Slog{l:l}
+}
+
+func (s *Slog) Close() {
+	s.l.Println("logger service shutdown")
+}
+
+func (s *Slog) Call(m Msg) error {
+	s.l.Println(string(m.data))
+	return nil
 }
